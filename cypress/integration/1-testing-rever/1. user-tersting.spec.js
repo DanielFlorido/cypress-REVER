9154dc46366c8b9994a50134675578bcb7359e68
Daniel Florido <d.florido@javeriana.edu.co>
2024-05-14 11:06:01 -0500

    Update 1. user-tersting.spec.js

--- cypress/integration/1-testing-rever/1. user-tersting.spec.js
+++ cypress/integration/1-testing-rever/1. user-tersting.spec.js
@@ -10,6 +10,11 @@ describe('user', () => {
         cy.get('input#\\:r1\\:').should('exist');
         cy.get('input#\\:r1\\:').type('d@cosa.com');
         cy.get('button[type="submit"]').click();
+        cy.get('.flex.flex-col.gap-4.border-solid.border-grey-3')  // Selecciona el contenedor principal que contiene los elementos de la lista de productos
+        .should('exist')  // Verifica que el contenedor exista en la página
+        .children('.sc-dtInlm.hjZkTt')  // Selecciona todos los elementos hijos que representan cada producto en la lista
+            .should('have.length', 2);  // Verifica que haya exactamente dos elementos que representen dos productos en la lista
+
     })
     
     it('Product Search by bad email', () => {
