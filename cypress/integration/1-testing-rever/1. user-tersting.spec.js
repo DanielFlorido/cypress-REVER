af0213e3cd5858f9f2bfbf32c0bfd8845118af9e
Daniel Florido <d.florido@javeriana.edu.co>
2024-05-14 10:58:46 -0500

    Update 1. user-tersting.spec.js

--- cypress/integration/1-testing-rever/1. user-tersting.spec.js
+++ cypress/integration/1-testing-rever/1. user-tersting.spec.js
@@ -4,8 +4,20 @@ describe('user', () => {
         cy.visit('/')
     })
 
-    it('try', () => {
-        cy.get('label[for=":r0:"]').should('exist');
-        cy.get('label[for=":r0:"]').type('hello');
-    })     
+    it('Product Search by Order Number', () => {
+        cy.get('input#\\:r0\\:').should('exist');
+        cy.get('input#\\:r0\\:').type('123456');
+        cy.get('input#\\:r1\\:').should('exist');
+        cy.get('input#\\:r1\\:').type('d@cosa.com');
+        cy.get('button[type="submit"]').click();
+    })
+    
+    it('Product Search by bad email', () => {
+        cy.get('input#\\:r0\\:').should('exist');
+        cy.get('input#\\:r0\\:').type('123456');
+        cy.get('input#\\:r1\\:').should('exist');
+        cy.get('input#\\:r1\\:').type('d@cosa');
+        cy.get('button[type="submit"]').click();
+        cy.get('label[for= ":r1:-helper-text"]').should('exist');
+    })
 })
